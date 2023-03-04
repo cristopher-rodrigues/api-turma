03baa93b3c25c25959db832cad67ab5695f45d03
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2023-03-04 11:53:33 -0300

    more thigns

---
+++ TODO.md
@@ -0,0 +1,1 @@
+1) add doc using https://swagger.io/
--- index.js
+++ index.js
@@ -1,11 +1,66 @@
+import bodyParser from "body-parser";
 import express from "express"; // commun <---
+import cors from "cors";
 // const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const teams = {
+  gremio: ["suarez", "pedrinho", "josé"],
+  internacional: ["carlitos", "ruan", "marquinhos"],
+};
+
+app.use(cors());
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/teams/:name", (req, res) => {
+  const players = teams[req.params.name];
+
+  if (players) {
+    return res.json({
+      players,
+    });
+  }
+
+  return res.status(404).json({
+    message: "team not found!",
+  });
 });
+
+app.get("/find-players", (req, res) => {
+  return res.render("index");
+});
+
+// MVC <----
+// app.post("/find-players", (req, res) => {
+//   const players = teams[req.body.team];
+
+//   if (players) {
+//     return res.write(
+//       `
+//         <h1>
+//             selecione o seu time
+//         </h1>
+//         <form method='POST'>
+//             <select name='team'>
+//                 <option value='gremio'>Gremio</option>
+//                 <option value='internacional'>Internacional</option>
+//             </select>
+//             <button type='submit'>submit</button>
+//         </form>
+//         <ul>
+//         ${players.map((p) => `<li>${p}</li>`)}
+//         </ul>
+//         `
+//     );
+//   }
+
+//   throw new Error("wtf");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
